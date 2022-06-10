f50ec0681aef5246a822f2fd5d78767db1794514
Tristan Carel <tcarel@umass.edu>
2022-06-10 19:23:19 -0400

    added a second player currently for comparing various times

--- data reader.js
+++ data reader.js
@@ -1,18 +1,29 @@
 class dataReader {
 
     constructor(){
-        this.playerTimes = {}
+        this.player1Times = {};
+        this.player2Times = {};
     }
 
 
     //current adds a time to the object with list of playerTimes taking an object as input with certain fields filled in
-    //addPlayerTime(input: {track: str, time: str, lap1: str, lap2: str, lap3: str}):
-    addPlayerTime(input) {
+    //addPlayerTime(player: number, input: {track: str, time: str, lap1: str, lap2: str, lap3: str}):
+    addPlayerTime(player, input) {
         let curTrack = input.track;
-        this.playerTimes[curTrack].time = input.time;
-        this.playerTimes[curTrack].lap1 = input.lap1;
-        this.playerTimes[curTrack].lap2 = input.lap2;
-        this.playerTimes[curTrack].lap3 = input.lap3;
+        if (player === 1){
+            this.player1Times[curTrack].time = input.time;
+            this.player1Times[curTrack].lap1 = input.lap1;
+            this.player1Times[curTrack].lap2 = input.lap2;
+            this.player1Times[curTrack].lap3 = input.lap3;
+        }
+        else if (player === 2) {
+            this.player2Times[curTrack].time = input.time;
+            this.player2Times[curTrack].lap1 = input.lap1;
+            this.player2Times[curTrack].lap2 = input.lap2;
+            this.player2Times[curTrack].lap3 = input.lap3;
+        }
     }
+
+
 
 }
